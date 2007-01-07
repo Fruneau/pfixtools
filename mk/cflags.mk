72ae8ee27f8277beda672104722ef3bdd86c8da9
Pierre Habouzit <madcoder@debian.org>
2007-01-08 00:34:43 +0100

    initial import.
    Signed-off-by: Pierre Habouzit <madcoder@debian.org>

---
+++ mk/cflags.mk
@@ -0,0 +1,85 @@
+##############################################################################
+#          postlicyd: a postfix policy daemon with a lot of features         #
+#          ~~~~~~~~~                                                         #
+#  ________________________________________________________________________  #
+#                                                                            #
+#  Redistribution and use in source and binary forms, with or without        #
+#  modification, are permitted provided that the following conditions        #
+#  are met:                                                                  #
+#                                                                            #
+#  1. Redistributions of source code must retain the above copyright         #
+#     notice, this list of conditions and the following disclaimer.          #
+#  2. Redistributions in binary form must reproduce the above copyright      #
+#     notice, this list of conditions and the following disclaimer in the    #
+#     documentation and/or other materials provided with the distribution.   #
+#  3. The names of its contributors may not be used to endorse or promote    #
+#     products derived from this software without specific prior written     #
+#     permission.                                                            #
+#                                                                            #
+#  THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND   #
+#  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE     #
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR        #
+#  PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS    #
+#  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR    #
+#  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF      #
+#  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  #
+#  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN   #
+#  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)   #
+#  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF    #
+#  THE POSSIBILITY OF SUCH DAMAGE.                                           #
+##############################################################################
+
+GCCVERSION:=$(shell $(CC) -dumpversion)
+GCCMACHINE:=$(shell $(CC) -dumpmachine)
+OBJSUFFIX:=-$(GCCMACHINE:-linux-gnu=)-$(GCCVERSION)
+
+ifneq (,$(filter 4.%,$(GCCVERSION)))
+  GCC4=1
+endif
+
+LDFLAGS += -Wl,--warn-common
+
+# Use pipes and not temp files.
+CFLAGS += -pipe
+# optimize even more
+CFLAGS += -O2
+# let the type char be unsigned by default
+CFLAGS += -funsigned-char
+CFLAGS += -fstrict-aliasing
+# turn on all common warnings
+CFLAGS += -Wall
+# turn on extra warnings
+CFLAGS += $(if $(GCC4),-Wextra,-W)
+# treat warnings as errors
+CFLAGS += -Werror
+CFLAGS += -Wchar-subscripts
+# warn about undefined preprocessor identifiers
+CFLAGS += -Wundef
+# warn about local variable shadowing another local variable
+CFLAGS += -Wshadow
+# warn about casting of pointers to increased alignment requirements
+CFLAGS += -Wcast-align
+# make string constants const
+CFLAGS += -Wwrite-strings
+# warn about implicit conversions with side effects
+# fgets, calloc and friends take an int, not size_t...
+#CFLAGS += -Wconversion
+# warn about comparisons between signed and unsigned values
+CFLAGS += -Wsign-compare
+# warn about unused declared stuff
+CFLAGS += -Wunused
+# don not warn about unused return value
+CFLAGS += -Wno-unused-value
+# warn about variable use before initialization
+CFLAGS += -Wuninitialized
+# warn about pointer arithmetic on void* and function pointers
+CFLAGS += -Wpointer-arith
+# warn about multiple declarations
+CFLAGS += -Wredundant-decls
+# warn if the format string is not a string literal
+CFLAGS += -Wformat-nonliteral
+# do not warn about strftime format with y2k issues
+CFLAGS += -Wno-format-y2k
+# barf if we change constness
+#CFLAGS += -Wcast-qual
+
---
+++ policy.h
@@ -0,0 +1,82 @@
+/******************************************************************************/
+/*          postlicyd: a postfix policy daemon with a lot of features         */
+/*          ~~~~~~~~~                                                         */
+/*  ________________________________________________________________________  */
+/*                                                                            */
+/*  Redistribution and use in source and binary forms, with or without        */
+/*  modification, are permitted provided that the following conditions        */
+/*  are met:                                                                  */
+/*                                                                            */
+/*  1. Redistributions of source code must retain the above copyright         */
+/*     notice, this list of conditions and the following disclaimer.          */
+/*  2. Redistributions in binary form must reproduce the above copyright      */
+/*     notice, this list of conditions and the following disclaimer in the    */
+/*     documentation and/or other materials provided with the distribution.   */
+/*  3. The names of its contributors may not be used to endorse or promote    */
+/*     products derived from this software without specific prior written     */
+/*     permission.                                                            */
+/*                                                                            */
+/*  THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND   */
+/*  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE     */
+/*  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR        */
+/*  PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS    */
+/*  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR    */
+/*  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF      */
+/*  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS  */
+/*  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN   */
+/*  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)   */
+/*  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF    */
+/*  THE POSSIBILITY OF SUCH DAMAGE.                                           */
+/******************************************************************************/
+
+/*
+ * Copyright (C) 2006 Pierre Habouzit
+ */
+
+#ifndef POSTLICYD_POLICY_H
+#define POSTLICYD_POLICY_H
+
+enum protocol_state {
+    STATE_CONNECT,
+    STATE_HELO, /* or EHLO */
+    STATE_MAIL,
+    STATE_RCPT,
+    STATE_DATE,
+    STATE_EOM,
+    STATE_VRFY,
+    STATE_ETRN,
+};
+
+typedef struct policy_request {
+    unsigned ready : 1;
+    unsigned state : 4;
+    unsigned esmtp : 1;
+
+    const char *helo_name;
+    const char *queue_id;
+    const char *sender;
+    const char *recipient;
+    const char *recipient_count;
+    const char *client_address;
+    const char *client_name;
+    const char *rclient_name;
+    const char *instance;
+
+    /* postfix 2.2+ */
+    const char *sasl_method;
+    const char *sasl_username;
+    const char *sasl_sender;
+    const char *size;
+    const char *ccert_subject;
+    const char *ccert_issuer;
+    const char *ccsert_fingerprint;
+
+    /* postfix 2.3+ */
+    const char *encryption_protocol;
+    const char *encryption_cipher;
+    const char *encryption_keysize;
+    const char *etrn_domain;
+} policy_request;
+
+
+#endif
