9c431239425204c8ebff67dacc191c576721ac5b
Florent Bruneau <florent.bruneau@polytechnique.org>
2009-09-03 23:03:54 +0200

    Add license and a short manual in tst-spf.
    Signed-off-by: Florent Bruneau <florent.bruneau@polytechnique.org>

--- common/tst-spf.c
+++ common/tst-spf.c
@@ -1,3 +1,51 @@
+/******************************************************************************/
+/*          pfixtools: a collection of postfix related tools                  */
+/*          ~~~~~~~~~                                                         */
+/*  ________________________________________________________________________  */
+/*                                                                            */
+/*  Redistribution and use in source and binary forms, with or without        */
+/*  modification, are permitted provided that the following conditions        */
+/*  are met:                                                                  */
+/*                                                                            */
+/*  1. Redistributions of source code must retain the above copyright         */
+/*     notice, this list of conditions and the following disclaimer.          */
+/*  2. Redistributions in binary form must reproduce the above copyright      */
+/*     notice, this list of conditions and the following disclaimer in the    */
+/*     documentation and/or other materials provided with the distribution.   */
+/*  3. The names of its contributors may not be used to endorse or promote    */
+/*     products derived from this software without specific prior written     */
+/*     permission.                                                            */
+/*                                                                            */
+/*  THIS SOFTWARE IS PROVIDED BY THE CONTRIBUTORS ``AS IS'' AND ANY EXPRESS   */
+/*  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED         */
+/*  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE    */
+/*  DISCLAIMED.  IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY         */
+/*  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL        */
+/*  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS   */
+/*  OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)     */
+/*  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,       */
+/*  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN  */
+/*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE           */
+/*  POSSIBILITY OF SUCH DAMAGE.                                               */
+/*                                                                            */
+/*   Copyright (c) 2006-2009 the Authors                                      */
+/*   see AUTHORS and source files for details                                 */
+/******************************************************************************/
+
+/*
+ * Copyright © 2009 Florent Bruneau
+ */
+
+/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! *
+ *  To run the test, you must have a resolv.conf in your current directoy with *
+ *  the following content:                                                     *
+ *                                                                             *
+ *  search example.com                                                         *
+ *  nameserver 88.163.156.52                                                   *
+ *                                                                             *
+ *  The IP of the nameserver may change in the future.                         *
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
+
 #include "spf.h"
 #include "server.h"
 
@@ -240,7 +288,7 @@ static void spf_test_next(spf_test_t* current) {
 int main(int argc, char *argv[])
 {
     dns_use_local_conf("resolv.conf");
-    log_level = LOG_DEBUG;
+    log_level = LOG_NOTICE;
     spf_test_next(NULL);
     return server_loop(NULL, NULL, NULL, NULL, NULL);
 }
